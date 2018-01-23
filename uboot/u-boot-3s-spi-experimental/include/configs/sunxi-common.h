4b4bde071b1bd12d66287951a2ab3607927b428d
suda-morris <362953310@qq.com>
2018-01-23 13:50:32 +0800

    add how-to replace u-boot logo

--- uboot/u-boot-3s-spi-experimental/include/configs/sunxi-common.h
+++ uboot/u-boot-3s-spi-experimental/include/configs/sunxi-common.h
@@ -315,6 +315,8 @@ extern int soft_i2c_gpio_scl;
 #define CONFIG_VIDEO_SUNXI
 
 #define CONFIG_VIDEO_LOGO
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_HIDE_LOGO_VERSION
 #define CONFIG_VIDEO_STD_TIMINGS
 #define CONFIG_I2C_EDID
 #define VIDEO_LINE_LEN (pGD->plnSizeX)
